0c0ec10ea8d9cb6c7654ffe655a91fdc7cb002c4
pacannon <phillip.alan.cannon@gmail.com>
2016-05-18 19:55:34 -0500

    Created master branch

---
+++ Mmo2d/Mmo2d/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mmo2d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
