c0250903f25992ba932ebb3bd19458a13b84b748
pacannon <phillip.alan.cannon@gmail.com>
2016-05-19 01:08:52 -0500

    Fixed server

--- Mmo2d/Mmo2d/LocalServer.cs
+++ Mmo2d/Mmo2d/LocalServer.cs
@@ -12,10 +12,12 @@ namespace Mmo2d
     class LocalServer : IServer
     {
         public TcpListener TcpListener { get; set; }
+        public ConcurrentDictionary<NetworkStream, object> Streams { get; set; }
 
         public LocalServer()
         {
             ResponseQueue = new ConcurrentQueue<ServerResponse>();
+            Streams = new ConcurrentDictionary<NetworkStream, object>();
 
             var TcpListenerTask = new Task(() =>
             {
@@ -47,6 +49,7 @@ namespace Mmo2d
 
                         // Get a stream object for reading and writing
                         NetworkStream stream = client.GetStream();
+                        Streams.AddOrUpdate(stream, new object(), (a, b) => { return new object(); });
 
                         int i;
 
@@ -55,17 +58,13 @@ namespace Mmo2d
                         {
                             // Translate data bytes to a ASCII string.
                             data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                            Console.WriteLine("Received: {0}", data);
 
-                            // Process the data sent by the client.
-                            data = data.ToUpper();
-
-                            byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-                            // Send back a response.
-                            stream.Write(msg, 0, msg.Length);
-                            Console.WriteLine("Sent: {0}", data);
+                            ResponseQueue.Enqueue(new ServerResponse() { TypedCharacter = data.ToCharArray()[0] });
                         }
+
+                        object temp;
+
+                        while (!Streams.TryRemove(stream, out temp)) { }
 
                         // Shutdown and end connection
                         client.Close();
@@ -98,79 +97,17 @@ namespace Mmo2d
 
         public void SendMessage(ServerMessage message)
         {
-            ResponseQueue.Enqueue(new ServerResponse { TypedCharacter = message.TypedCharacter});
-        }
+            var typedCharacter = message.TypedCharacter;
 
-        public static void Main2()
-        {
-            TcpListener server = null;
-            try
+            foreach (var stream in Streams.Keys)
             {
-                // Set the TcpListener on port 13000.
-                Int32 port = 13000;
-                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(typedCharacter.ToString());
 
-                // TcpListener server = new TcpListener(port);
-                server = new TcpListener(localAddr, port);
-
-                // Start listening for client requests.
-                server.Start();
-
-                // Buffer for reading data
-                Byte[] bytes = new Byte[256];
-                String data = null;
-
-                // Enter the listening loop.
-                while (true)
-                {
-                    Console.Write("Waiting for a connection... ");
-
-                    // Perform a blocking call to accept requests.
-                    // You could also user server.AcceptSocket() here.
-                    TcpClient client = server.AcceptTcpClient();
-                    Console.WriteLine("Connected!");
-
-                    data = null;
-
-                    // Get a stream object for reading and writing
-                    NetworkStream stream = client.GetStream();
-
-                    int i;
-
-                    // Loop to receive all the data sent by the client.
-                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                    {
-                        // Translate data bytes to a ASCII string.
-                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                        Console.WriteLine("Received: {0}", data);
-
-                        // Process the data sent by the client.
-                        data = data.ToUpper();
-
-                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-                        // Send back a response.
-                        stream.Write(msg, 0, msg.Length);
-                        Console.WriteLine("Sent: {0}", data);
-                    }
-
-                    // Shutdown and end connection
-                    client.Close();
-                }
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-            finally
-            {
-                // Stop listening for new clients.
-                server.Stop();
+                // Send back a response.
+                stream.Write(msg, 0, msg.Length);
             }
 
-
-            Console.WriteLine("\nHit enter to continue...");
-            Console.Read();
+            ResponseQueue.Enqueue(new ServerResponse { TypedCharacter = typedCharacter });
         }
     }
 }
