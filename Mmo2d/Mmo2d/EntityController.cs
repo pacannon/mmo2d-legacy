9e6a7678ce97f2397b968fe42360fca8576415e2
pacannon <phillip.alan.cannon@gmail.com>
2016-11-14 01:27:00 -0600

    missed file

---
+++ Mmo2d/Mmo2d/EntityController.cs
@@ -0,0 +1,105 @@
+﻿using Mmo2d.UserCommands;
+
+namespace Mmo2d
+{
+    public class EntityController
+    {
+        public bool MoveUp { get; set; }
+        public bool MoveUpAtAll { get; set; }
+
+        public bool MoveDown { get; set; }
+        public bool MoveDownAtAll { get; set; }
+
+        public bool MoveLeft { get; set; }
+        public bool MoveLeftAtAll { get; set; }
+
+        public bool MoveRight { get; set; }
+        public bool MoveRightAtAll { get; set; }
+
+        public bool Attack { get; set; }
+        public bool AttackAtAll { get; set; }
+
+        public bool Jump { get; set; }
+        public bool JumpAtAll { get; set; }
+
+        public EntityController ApplyUserCommand(UserCommand userCommand)
+        {
+            var clone = (EntityController)this.MemberwiseClone();
+
+            var keyEventArgs = userCommand.KeyEventArgs;
+
+            if (keyEventArgs != null)
+            {
+                switch (keyEventArgs.Key)
+                {
+                    case OpenTK.Input.Key.W:
+
+                        if (keyEventArgs.KeyDown)
+                        {
+                            clone.MoveUpAtAll = true;
+                        }
+                        clone.MoveUp = keyEventArgs.KeyDown;
+                        break;
+
+                    case OpenTK.Input.Key.S:
+
+                        if (keyEventArgs.KeyDown)
+                        {
+                            clone.MoveDownAtAll = true;
+                        }
+                        clone.MoveDown = keyEventArgs.KeyDown;
+                        break;
+
+                    case OpenTK.Input.Key.A:
+
+                        if (keyEventArgs.KeyDown)
+                        {
+                            clone.MoveLeftAtAll = true;
+                        }
+                        clone.MoveLeft = keyEventArgs.KeyDown;
+                        break;
+
+                    case OpenTK.Input.Key.D:
+
+                        if (keyEventArgs.KeyDown)
+                        {
+                            clone.MoveRightAtAll = true;
+                        }
+                        clone.MoveRight = keyEventArgs.KeyDown;
+                        break;
+
+                    case OpenTK.Input.Key.Space:
+
+                        if (keyEventArgs.KeyDown)
+                        {
+                            clone.JumpAtAll = true;
+                        }
+                        clone.Jump = keyEventArgs.KeyDown;
+                        break;
+                }
+            }
+
+            if (userCommand.MousePressed.HasValue)
+            {
+                if (userCommand.MousePressed.Value)
+                {
+                    clone.AttackAtAll = true;
+                }
+
+                clone.Attack = userCommand.MousePressed.Value;
+            }
+
+            return clone;
+        }
+
+        public void Update()
+        {
+            MoveUpAtAll = false;
+            MoveDownAtAll = false;
+            MoveLeftAtAll = false;
+            MoveRightAtAll = false;
+            AttackAtAll = false;
+            JumpAtAll = false;
+        }
+    }
+}
